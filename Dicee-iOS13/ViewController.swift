c46f80569da8fce11d6d5c693e5f04e486d085f6
Derek Janis <dmjanis@gmail.com>
2020-06-23 22:42:35 -0400

    Through Lesson 44

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -13,18 +13,25 @@ class ViewController: UIViewController {
     @IBOutlet weak var diceImageViewOne: UIImageView!
     @IBOutlet weak var diceImageViewTwo: UIImageView!
     
+    var leftDiceNumber = 0
+    var rightDiceNumber = 5
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        diceImageViewOne.image = #imageLiteral(resourceName: "DiceSix")
-        diceImageViewTwo.image = #imageLiteral(resourceName: "DiceTwo")
         
     }
 
     //Change dice when Roll is pressed.
+    
     @IBAction func rollButtonPressed(_ sender: UIButton) {
-        diceImageViewOne.image = #imageLiteral(resourceName: "DiceFour")
-        diceImageViewTwo.image = #imageLiteral(resourceName: "DiceFour")
+        
+        diceImageViewOne.image = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")][leftDiceNumber]
+        
+        leftDiceNumber = leftDiceNumber + 1
+        
+        diceImageViewTwo.image = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")][rightDiceNumber]
+        
+        rightDiceNumber = rightDiceNumber - 1
     }
     
 }
